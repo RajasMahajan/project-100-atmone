fb45991ea7e8ba5d4f5b7970555b456be9d36c1e
RajasMahajan <73475718+RajasMahajan@users.noreply.github.com>
2022-01-30 21:16:08 +0530

    Add files via upload

---
+++ class100project.py
@@ -0,0 +1,21 @@
+class atm():
+    def __init__(self,pin,number,balance):
+        self.pin = pin
+        self.number = number
+        self.balance = balance
+    def signin1(self):
+        pinone = 7123
+        number2 = 121212
+        if self.pin == pinone & self.number == number2:
+            print(self.balance)
+balance1 = 5000
+
+tellcardnumber = input("Enter you cardnumberhere: ")
+
+tellpin = input("Enter you pin here: ")
+
+start = atm(tellpin,tellcardnumber,balance1)
+
+start.signin1()
+
+
